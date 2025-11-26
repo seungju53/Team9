ab33c45e7f837607fce2cd75a1b8444a1cbd15cf
jaehyoung Yang <20221056@edu.hanbat.ac.kr>
2025-11-26 16:41:22 +0900

    no message

--- main.py
+++ main.py
@@ -16,16 +16,99 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 ev3 = EV3Brick()
 
 
-left = Motor(Port.A)
-right = Motor(Port.D)
-
-left.run(150)
-right.run(150)
-
-wait(2000)
-
-left.stop()
-right.stop()
 
 # Write your program here.
 ev3.speaker.beep()
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.D)
+arm_motor = Motor(Port.B)
+robot = DriveBase(left_motor,right_motor,55.5,104)
+left_sensor = ColorSensor(Port.S1)
+ultra_sensor = UltrasonicSensor(Port.S3)
+object_detector = ColorSensor(Port.S2)
+right_sensor = ColorSensor(Port.S4)
+
+threshold = 50
+kp = 5
+
+ev3.speaker.beep()
+def left_line_following(speed, kp):
+    threshold = 50
+    left_reflection = left_sensor.reflection()
+    error = left_reflection - threshold
+    turn_rate = kp * error
+    robot.drive(speed,turn_rate)
+
+
+def right_line_following(speed, kp):
+    threshold = 50
+    right_reflection = right_sensor.reflection()
+    error = right_reflection - threshold
+    turn_rate = kp * error
+    robot.drive(speed,turn_rate)
+
+
+def n_move(n, direction = "right"):
+    for _ in range(n):
+        if direction == 'right':
+            while right_sensor.reflection() > 55:
+                left_line_following(100, 1.2)
+            while right_sensor.reflection() <= 55:
+                right_line_following(100, 1.2)
+        elif direction == 'left':
+            while left_sensor.reflection() > 50:
+                right_line_following(100, 1.2)
+            while left_sensor.reflection() <= 50:
+                left_line_following(100, 1.2)
+    
+    robot.stop()
+
+
+def grab_object():
+
+    arm_motor.run_until_stalled(200, then=Stop.COAST, duty_limit=80)
+def release_object():
+    arm_motor.run_until_stalled(-200,then=Stop.COAST,duty_limit=50)
+
+release_object()
+robot.straight(100)
+n_move(1,direction="left")
+while ultra_sensor.distance()>50:
+    left_line_following(100,1.2)
+robot.stop()
+robot.straight(40)
+
+grab_object()
+wait(500)
+object_color = object_detector.color()
+print("Detected object color",object_color)
+
+
+if object_color == Color.RED:
+    robot.turn(190)
+    n_move(1,direction='left')
+    robot.straight(40)
+    robot.turn(-95)
+    n_move(1,direction='right')
+    robot.straight(40)
+    robot.turn(95)
+    n_move(2,direction='right')
+    robot.straight(40)
+    release_object()
+
+elif object_color == Color.GREEN:
+    robot.turn(190)
+    n_move(1,direction='right')
+    robot.straight(50)
+    robot.turn(-95)
+    n_move(2,direction='left')
+    robot.straight(40)
+    robot.turn(95)
+    n_move(2,direction='left')
+    robot.straight(40)
+    release_object()
+
+
+
+
+
